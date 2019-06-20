621aba80a3ffd23dc0620d427df01d00c313e544
Jato Xiang <jiatao.xiang@mail.utoronto.ca>
2019-06-20 07:13:07 -0400

    boring game

--- javascript/keyboard.js
+++ javascript/keyboard.js
@@ -7,10 +7,28 @@ let ctx = canvas.getContext('2d');
 const defaultX = 200;
 const defaultY = 200;
 
+function Ball(x,y) {
+    this.x = x;
+    this.y = y;
+    this.speed = 5;
+    this.isMoving = true;
+    this.draw = function () {
+        ctx.beginPath();
+        ctx.rect(this.x, this.y, 10, 10);
+        ctx.fillStyle = 'black';
+        ctx.fill();
+    };
+    this.move = function () {
+      this.x += this.speed;
+      this.draw();
+    }
+}
+
 function People() {
     this.x = defaultX;
     this.y = defaultY;
-    this.speed = 20;
+    this.speed = 10;
+    this.balls = [];
 
     this.draw = function () {
         ctx.beginPath();
@@ -21,58 +39,79 @@ function People() {
         ctx.fillStyle = 'black';
         ctx.fill();
     };
+    this.hit = function () {
+        this.balls.push(new Ball(this.x + 5, this.y -10));
+        console.log(human1.balls);
+    };
     this.goingLeft = function () {
         this.x -= this.speed;
+        //this.draw();
     };
     this.goingRight = function () {
         this.x += this.speed;
+        //this.draw();
     };
     this.goingUp = function () {
-        this.y -= this.speed;
+        this.y -= this.speed*5;
+        // ctx.fillStyle='rgba(119,136,153,0.5)';
+        // ctx.fillRect(0,0,canvas.width,canvas.height);
+        // this.drawLeap();
     };
     this.goingDown = function () {
-        this.y += this.speed;
+        this.y += this.speed*5;
+        // this.drawLeap();
     }
 }
 
 let human1 = new People();
+
+function Leap() {
+    human1.goingUp();
+    setTimeout(function () {
+        human1.goingDown();
+    }, 200);
+}
+
 
 document.onkeydown=function (event) {
     const e = event;
     if (e && e.keyCode===37) {//left
         human1.goingLeft();
     }else if (e && e.keyCode===38){ //up
-        human1.goingUp();
-        // animateLeap();
+        Leap();
     }else if (e && e.keyCode===39){ //right
         human1.goingRight();
     }else if (e && e.keyCode===40) {//down
-        human1.goingDown();
+        // human1.goingDown();
+        human1.hit();
     }
 };
-// function animateLeap() {
-//     requestAnimationFrame(animate);
-//     let i;
-//     for( i = 0; i < 20; i ++) {
-//         ctx.fillStyle='rgba(119,136,153,0.5)';
-//         ctx.fillRect(0,0,canvas.width,canvas.height);
-//         human1.goingUp();
-//         human1.draw();
-//     }
-//     console.log("(my x: " + human1.x + ", my y: " + human1.y + ")");
-//     for(i = 0; i < 20; i ++) {
-//         ctx.fillStyle='rgba(119,136,153,0.5)';
-//         ctx.fillRect(0,0,canvas.width,canvas.height);
-//         human1.goingDown();
-//         human1.draw();
-//     }
-//     console.log("(my x: " + human1.x + ", my y: " + human1.y + ")");
-// }
+function getBallIndex(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (!array[i].isMoving){
+            return i;
+        }
+    }
+    return null;
+}
+
 function animate () {
     requestAnimationFrame(animate);
     ctx.fillStyle='rgba(119,136,153,0.5)';
     ctx.fillRect(0,0,canvas.width,canvas.height);
     human1.draw();
+    human1.balls.forEach(ball => {
+        if (ball.x > canvas.width - 200) {
+            ball.isMoving = false;
+        } else if (ball.isMoving){
+            ball.move();
+        }
+    });
+    let index = getBallIndex(human1.balls);
+    if (index != null) {
+        human1.balls.splice(index, index + 1);
+        console.log(human1.balls);
+    }
 }
 
 animate();
